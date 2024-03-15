d1ba864776e096bdf929db89dbb532f0b7299063
Victoria <adedayovicky123@gmail.com>
2024-03-15 17:47:07 +0100

    UPDATE: useMyNft

--- src/hooks/useMyNfts.js
+++ src/hooks/useMyNfts.js
@@ -4,82 +4,71 @@ import multicallAbi from "../constants/multicall.json";
 import { readOnlyProvider } from "../constants/providers";
 import { useEffect, useMemo, useState } from "react";
 import { useWeb3ModalAccount } from "@web3modal/ethers/react";
+import useNewOwner from "./useNewOwner";
 
 const useMyNfts = () => {
-    const { address } = useWeb3ModalAccount();
-    // const [data, setData] = useState([]);
+  const eventListener = useNewOwner();
 
-    const [data, setData] = useState({
-        address: [],
-        data: [],
-    });
-    const [isMintedId, setIsMintedId] = useState([]);
+  const { address } = useWeb3ModalAccount();
+  const [data, setData] = useState({
+    address: [],
+    data: [],
+  });
+  const [isMintedId, setIsMintedId] = useState([]);
 
-    const tokenIDs = useMemo(
-        () => [...Array.from({ length: 30 })].map((_, index) => index),
-        []
-    );
+  const tokenIDs = useMemo(
+    () => [...Array.from({ length: 30 })].map((_, index) => index),
+    []
+  );
 
-    useEffect(() => {
-        (async () => {
-            const itf = new ethers.Interface(erc721);
-            const calls = tokenIDs.map((x) => ({
-                target: import.meta.env.VITE_contract_address,
-                callData: itf.encodeFunctionData("ownerOf", [x]),
-            }));
+  useEffect(() => {
+    (async () => {
+      const itf = new ethers.Interface(erc721);
+      const calls = tokenIDs.map((x) => ({
+        target: import.meta.env.VITE_contract_address,
+        callData: itf.encodeFunctionData("ownerOf", [x]),
+      }));
 
-            const multicall = new ethers.Contract(
-                import.meta.env.VITE_multicall_address,
-                multicallAbi,
-                readOnlyProvider
-            );
+      const multicall = new ethers.Contract(
+        import.meta.env.VITE_multicall_address,
+        multicallAbi,
+        readOnlyProvider
+      );
 
-            const callResults = await multicall.tryAggregate.staticCall(
-                false,
-                calls
-            );
+      const callResults = await multicall.tryAggregate.staticCall(false, calls);
 
-            const validResponsesIndex = [];
-            const validResponses = callResults.filter((x, i) => {
-                if (x[0] === true) {
-                    validResponsesIndex.push(i);
-                    return true;
-                }
-                return false;
-            });
+      const validResponsesIndex = [];
+      const validResponses = callResults.filter((x, i) => {
+        if (x[0] === true) {
+          validResponsesIndex.push(i);
+          return true;
+        }
+        return false;
+      });
 
-            setIsMintedId(validResponsesIndex);
+      setIsMintedId(validResponsesIndex);
 
-            const decodedResponses = validResponses.map((x) =>
-                itf.decodeFunctionResult("ownerOf", x[1])
-            );
+      const decodedResponses = validResponses.map((x) =>
+        itf.decodeFunctionResult("ownerOf", x[1])
+      );
 
-            const ownedTokenIds = [];
+      const ownedTokenIds = [];
 
-            decodedResponses.forEach((addr, index) => {
-                tokenIDs.indexOf(validResponsesIndex[index]);
+      decodedResponses.forEach((addr, index) => {
+        tokenIDs.indexOf(validResponsesIndex[index]);
+        if (index !== -1) {
+          tokenIDs[validResponsesIndex[index]] = String(addr).toLowerCase();
+        }
 
-                if (index !== -1) {
-                    tokenIDs[validResponsesIndex[index]] =
-                        String(addr).toLowerCase();
-                }
+        if (String(addr).toLowerCase() === String(address).toLowerCase())
+          ownedTokenIds.push(validResponsesIndex[index]);
+      });
 
-                if (
-                    String(addr).toLowerCase() === String(address).toLowerCase()
-                )
-                    ownedTokenIds.push(validResponsesIndex[index]);
-            });
+      setData((prev) => ({ ...prev, address: tokenIDs, data: ownedTokenIds }));
+    })();
+  }, [address, tokenIDs, eventListener]);
 
-            setData(ownedTokenIds);
-            setData((prev) => ({
-                ...prev,
-                address: tokenIDs,
-                data: ownedTokenIds,
-            }));
-        })();
-    }, [isMintedId, address, tokenIDs]);
-
-    return data;
+  return { data, isMintedId };
 };
 
 export default useMyNfts;
